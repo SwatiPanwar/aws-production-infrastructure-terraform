b0d0e2ca491ab25a906124fda86a8d540c5231d8
SwatiPanwar <70883018+SwatiPanwar@users.noreply.github.com>
2026-04-19 23:49:29 +0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  # Amazon Linux 2 (US East 1)
+  default = "ami-0c02fb55956c7d316"
+}
